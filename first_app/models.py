02d90de4e50599eaeb9d1b330301599bf3ce4b01
mac-rifat <macrifat@gmail.com>
2022-11-16 14:33:01 +0100

    create model

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Members(models.Model):
+    first_name = models.CharField(max_length= 50)
+    last_name = models.CharField(max_length= 50)
+    phone_number = models.IntegerField()
+    email = models.EmailField()
+    
+    
+    def __str__(self) -> str:
+        return str(self.pk) + " "+ self.first_name+ " "+ self.last_name
